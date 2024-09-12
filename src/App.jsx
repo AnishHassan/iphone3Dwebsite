1cca017e1c1f8cb5dc3b9d860ebf8022720a7e91
Anish Hassan <anish.hassanworks@gmail.com>
2024-09-12 16:19:35 +0500

    Features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./Components/Features"
 import Hero from "./Components/Hero"
 import Highlights from "./Components/Highlights"
 import Model from "./Components/Model"
@@ -13,6 +14,7 @@ const App = () => {
         <Hero />
         <Highlights />
         <Model />
+        <Features/>
       </main>
     </>
   )
